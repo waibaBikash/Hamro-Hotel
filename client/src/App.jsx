c54816217c170d74ebde45ae7b5181ec5a356799
waibaBikash <kavrelibikash@gmail.com>
2025-05-04 22:56:29 +1000

    Add Navbar component with responsive design and scroll effects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar';
+
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes('owner');
   return (
     <div>
-      
+      { !isOwnerPath && <Navbar /> }
     </div>
   )
 }

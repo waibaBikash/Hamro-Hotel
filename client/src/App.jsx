fa77dc488eda2456af7c3192955b6485c49e053d
waibaBikash <kavrelibikash@gmail.com>
2025-05-05 21:32:59 +1000

    Add Home page and Hero component; update App to include routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 
 const App = () => {
@@ -9,6 +10,11 @@ const App = () => {
   return (
     <div>
       { !isOwnerPath && <Navbar /> }
+        <div className='min-h-[70vh]'>
+           <Routes>
+             <Route path='/' element={<Home />} />
+           </Routes>
+        </div>
     </div>
   )
 }
---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-center justify-center px-6 md:px-16 lg:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+       
+    </div>
+  )
+}
+
+export default Hero

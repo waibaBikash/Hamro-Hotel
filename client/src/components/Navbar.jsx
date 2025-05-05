2416d5e1c802faaeced3f654946ea2d169fd6563
waibaBikash <kavrelibikash@gmail.com>
2025-05-05 22:18:00 +1000

    Add Clerk authentication integration; update Navbar for login functionality and configure ClerkProvider in main.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {assets} from '../assets/assets'
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
   const navLinks = [
@@ -14,6 +15,9 @@ const Navbar = () => {
 
   const [isScrolled, setIsScrolled] = React.useState(false);
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const {openSingIn} = useClerk();
+  const {user} = useUser();
+
 
   React.useEffect(() => {
       const handleScroll = () => {
@@ -48,7 +52,7 @@ const Navbar = () => {
               {/* Desktop Right */}
               <div className="hidden md:flex items-center gap-4">
                   <img src={assets.searchIcon} alt="search" className= {`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
-                  <button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
+                  <button onClick={openSingIn} className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
                       Login
                   </button>
               </div>
@@ -74,7 +78,7 @@ const Navbar = () => {
                       Dashboard
                   </button>
 
-                  <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                  <button onClick={openSingIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                       Login
                   </button>
               </div>

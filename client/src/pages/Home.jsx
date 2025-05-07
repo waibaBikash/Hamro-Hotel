161166ade2267160bbb65cf825f293cdfbdb4eba
waibaBikash <kavrelibikash@gmail.com>
2025-05-07 21:58:42 +1000

    Add FeatureDestination and HotelCard components to Home page; implement room display logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeatureDestination from '../components/FeatureDestination'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeatureDestination />
     </>
   )
 }

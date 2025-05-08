980d3457bba277cec99a623553e9a91529cf84b3
waibaBikash <kavrelibikash@gmail.com>
2025-05-08 21:08:21 +1000

    Refactor HotelCard component for improved code readability and formatting

--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -4,10 +4,13 @@ import { assets } from '../assets/assets'
 
 const HotelCard = ({room, index}) => {
   return (
-    <Link  to={'/rooms/' + room._id} onClick={()=> scrollTo(0,0)} key={room._id} className='relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]' >
+    <Link  to={'/rooms/' + room._id} 
+    onClick={()=> scrollTo(0,0)} key={room._id}
+     className='relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]' >
       <img src={room.images[0]} alt="" />
       {
-        index % 2 === 0 && <p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full '>Best Seller</p>
+        index % 2 === 0 && <p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full '>
+          Best Seller</p>
       }
       
       <div className='p-4 pt-5'>

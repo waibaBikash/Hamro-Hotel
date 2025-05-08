884d73ffae94d287a4492177363cdabfa243aa9d
waibaBikash <kavrelibikash@gmail.com>
2025-05-08 20:00:39 +1000

    Add ExclusiveOffers component to Home page; implement layout and offer display logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeatureDestination from '../components/FeatureDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeatureDestination />
+      <ExclusiveOffers />
     </>
   )
 }

fa92c4739f39b1ed17d4c6b6b054432785a285b3
waibaBikash <kavrelibikash@gmail.com>
2025-05-11 20:30:09 +1000

    Refactor MyBookings component for improved readability and formatting

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -8,7 +8,10 @@ const MyBookings = () => {
   return (
     <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
 
-       <Title title='My Bookings' subTitle='Easily manage your past, current, and upcoming hotel reservations in one place. Plan your trips seamlessly with just a few clicks' align='left' />
+       <Title 
+        title='My Bookings'
+        subTitle='Easily manage your past, current, and upcoming hotel reservations in one place. Plan your trips seamlessly with just a few clicks' align='left' 
+        />
 
        <div className='max-w-6xl mt-8 w-full text-gray-800'>
           <div className='hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3'>
@@ -17,7 +20,8 @@ const MyBookings = () => {
              <div className='w-1/3'>Payments</div>
           </div>
           {bookings.map((booking)=>(
-             <div key={booking._id} className='grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t'>
+             <div key={booking._id}
+              className='grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t'>
                 {/* Hotels Details */}
                 <div className='flex flex-col md:flex-row'>
                   <img src={booking.room.images[0]} alt="hotel-img" className='min-md:w-44 rounded shadow object-cover' />

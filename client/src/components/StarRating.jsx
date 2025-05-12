25e3f6d6ee7106a4053f83000bd8c26f03f75bd4
waibaBikash <kavrelibikash@gmail.com>
2025-05-12 10:02:29 +1000

    Refactor StarRating component for improved code readability

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -6,7 +6,10 @@ const StarRating = ({rating = 4}) => {
     <>
       {
         Array(5).fill('').map((_, index) => (
-           <img src={rating > index ? assets.starIconFilled : assets.starIconOutlined} alt="star-icon" className='w-4.5 h-4.5' />
+           <img 
+           src={rating > index ? assets.starIconFilled : assets.starIconOutlined}
+           alt="star-icon"
+           className='w-4.5 h-4.5' />
         ))
       }
     </>

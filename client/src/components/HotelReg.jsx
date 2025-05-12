f06d6e8f491eba31f6f90507479bdf074bc52874
waibaBikash <kavrelibikash@gmail.com>
2025-05-12 13:05:54 +1000

    Add hotel registration form with fields for name, contact, address, and city selection

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, cities } from '../assets/assets'
 
 const HotelReg = () => {
   return (
@@ -11,6 +11,34 @@ const HotelReg = () => {
         <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
            <img src={assets.closeIcon} alt="close-icon" className='absolute top-4 ring-4 h-4 w-4 cursor-pointer' />
            <p className='text-2xl font-semibold mt-6'>Register Your Hotel</p>
+            <div className='w-full mt-4'>
+              {/* Hotel Name */}
+               <label htmlFor="name" className='font-medium text-gray-500'>Hotel Name</label>
+               <input id='name' type="text" placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500' required />
+            </div>
+                   {/* Hotel Contact */}
+            <div className='w-full mt-4'>
+               <label htmlFor="contact" className='font-medium text-gray-500'>Phone</label>
+               <input id='contact' type="text" placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500' required />
+            </div>
+                   {/* Hotel Address */}
+            <div className='w-full mt-4'>
+               <label htmlFor="address" className='font-medium text-gray-500'>Address</label>
+               <input id='address' type="text" placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500' required />
+            </div>
+            {/* Select City Drop Down */}
+             <div className='w-full mt-4 max-w-60 mr-auto'>
+               <label htmlFor="city" className='font-medium text-gray-500'>City</label>
+               <select id="city" className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
+                 <option value="">Select City</option>
+                 {cities.map((city)=>(
+                   <option key={city} value={city}>
+                    {city}
+                   </option>
+                 ))}
+               </select>
+             </div>
+              <button className='bg-indigo-500 hover:bg-indigo-600 transition-all text-white mr-auto px-6 py-2 rounded cursor-pointer mt-6'>Register</button>
         </div>
       </form>
 

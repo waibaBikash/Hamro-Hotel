a034b365f6534e18b375a26af3890783b1c89605
waibaBikash <kavrelibikash@gmail.com>
2025-05-14 22:29:26 +1000

    Add recent bookings section to Dashboard with user details and payment status

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -27,6 +27,43 @@ const Dashboard = () => {
            </div>
            
         </div>
+
+              {/* Recent Bookings */}
+              <h2 className='text-xl text-blue-950/70 font-medium mb-5'>Recnet Bookings</h2>
+            <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
+
+               <table className='w-full'>
+                <thead className='bg-gray-50'>
+                 <tr>
+                     <th className='py-3 px-4 text-gray-800 font-medium'>User Name</th>
+                     <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Room Name</th>
+                     <th className='py-3 px-4 text-gray-800 font-medium text-center'>Total Amount</th>
+                     <th className='py-3 px-4 text-gray-800 font-medium text-center'>Payment Status</th>
+                 </tr>
+               </thead>
+
+                <tbody className='text-sm'>
+                  {dashboardData.bookings.map((tiem, index)=>(
+                    <tr key={index} className='border-b border-gray-200'>
+                       <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                         {item.user.userName}
+                       </td>
+                       <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                         {item.room.rootType}
+                       </td>
+                       <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
+                         $ {item.totalPrice}
+                       </td>
+                        <td className='py-3 px-4 border-t border-gray-300 flex'>
+                             <button className={`py-1 px-3 text-xs rounded-full mx-auto ${item.isPaid ? 'bg-green-200 text-green-600' : 'bg-amber-100 text-yellow-600'}`}>
+                              {item.isPaid ? 'Completed' : 'Pending' }
+                             </button>
+                        </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
     </div>
   )
 }

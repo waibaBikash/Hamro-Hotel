8a1411df86a88a547d8ef4b063cf7e54615717f7
waibaBikash <kavrelibikash@gmail.com>
2025-05-22 14:25:23 +1000

    Add room creation functionality with Cloudinary image upload and implement upload middleware

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,17 +1,35 @@
-import Hotel from "../models/Hotel";
-
+import Hotel from "../models/Hotel.js";
+import { v2 as cloudinary } from "cloudinary";
+import Room from "../models/Room.js";
 // Api to create a ner room for a hotel
 export const createRoom = async (req, res) => {
   try {
     const { roomType, pricePerNight, aminities} = req.body;
-    const hotel = await Hotel.findById(owner: req.auth.userId);
+    const hotel = await Hotel.findById({owner: req.auth.userId});
 
-    if(!hotel){
+    if(!hotel)
       return res.json({success: false, message: "Hotel not found"});
-    }
-    
+
+      // upload image to cloudinary
+
+      const uploadImages = req.files.map(async (file) => {
+      const response = await cloudinary.uploader.upload(file.path);
+      return response.secure_url;
+      })
+
+      // wait for all images to be uploaded
+      const images = await Promise.all(uploadImages);
+
+      await Room.create({
+        hotel: hotel._id,
+        roomType,
+        pricePerNight: +pricePerNight,
+        aminities: JSON.parse(aminities),
+        images
+      })
+  res.json({success: true, message: "Room created successfully"});
   } catch (error) {
-    
+    res.json({success: false, message: error.message});
   }
 }
 

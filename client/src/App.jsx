96de2292a44fb24b1414eeac28875d38f209ec88
waibaBikash <kavrelibikash@gmail.com>
2025-05-25 21:43:10 +1000

    Fix conditional rendering of HotelReg component based on AppContext state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -12,16 +12,20 @@ import Dashboard from './pages/hotelOwner/Dashboard';
 import AddRoom from './pages/hotelOwner/AddRoom';
 import ListRoom from './pages/hotelOwner/ListRoom';
 import {Toaster} from 'react-hot-toast';
+import { useAppcontext } from './context/AppContext';
 
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes('owner');
+  
+  const {showHotelReg} = useAppcontext();
+
   return (
     <div>
       <Toaster />
       { !isOwnerPath && <Navbar /> }
-      { false && <HotelReg />}
+      { showHotelReg && <HotelReg />}
         <div className='min-h-[70vh]'>
            <Routes>
              <Route path='/' element={<Home />} />

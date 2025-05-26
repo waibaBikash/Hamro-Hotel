f9ab3f9281061add5c848fa6f7d046abca028b4c
waibaBikash <kavrelibikash@gmail.com>
2025-05-26 20:23:56 +1000

    Improve formatting and readability of Testimonial component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,13 @@ import { useClerk, UserButton } from '@clerk/clerk-react';
 import { useAppcontext } from '../context/AppContext';
 
 const BookIcon = () => (
-    <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
-    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+    <svg className="w-4 h-4 text-gray-700"
+     aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
+     width="24" height="24" fill="none" viewBox="0 0 24 24" >
+    <path stroke="currentColor" strokeLinecap="round" 
+    strokeLinejoin="round" 
+    strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" 
+    />
 </svg>
 )
 
@@ -18,7 +23,6 @@ const Navbar = () => {
       { name: 'About', path: '/' },
   ];
 
-  
 
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -108,8 +112,9 @@ const Navbar = () => {
               {
                 user && <UserButton>
                     <UserButton.MenuItems>
-                        <UserButton.Action label="My Bookings" labelIcon={<BookIcon/>}
-                         onClick={()=>navigate('/my-bookings')}>
+                        <UserButton.Action label="My Bookings" 
+                        labelIcon={<BookIcon/>}
+                        onClick={()=>navigate('/my-bookings')}>
     
                         </UserButton.Action>
                     </UserButton.MenuItems>
--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -11,9 +11,11 @@ const Testimonial = () => {
        
       <div className="flex flex-wrap items-center gap-6 mt-20">
                 {testimonials.map((testimonial) => (
-                    <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow">
+                    <div key={testimonial.id}
+                     className="bg-white p-6 rounded-xl shadow">
                         <div className="flex items-center gap-3">
-                            <img className="w-12 h-12 rounded-full" src={testimonial.image} alt={testimonial.name} />
+                            <img className="w-12 h-12 rounded-full" 
+                            src={testimonial.image} alt={testimonial.name} />
                             <div>
                                 <p className="font-playfair text-xl">{testimonial.name}</p>
                                 <p className="text-gray-500">{testimonial.address}</p>

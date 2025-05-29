b46596cf1da0a4a7216bc1c1fb789f8020f7d3f9
waibaBikash <kavrelibikash@gmail.com>
2025-05-29 21:39:08 +1000

    Fix formatting issues in AllRooms component by improving indentation and line breaks for better readability

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -48,17 +48,24 @@ const AllRooms = () => {
   ]
 
   return (
-    <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+    <div
+      className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
        <div>
          <div className='flex flex-col items-start text-left'>
            <h1 className='font-plafair text-4xl md:test-[40px]'>Hotel Rooms</h1>
-           <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enchanace your stay and create unforgetable memories.</p>
+           <p 
+            className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>
+              Take advantage of our limited-time offers and special packages to 
+              enchanace your stay and create unforgetable memories.</p>
          </div>
           
           {roomsDummyData.map((room)=>(
-            <div key={room._id} className='flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0'>
-              <img onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}}
-               src={room.images[0]} alt="Hotel-img" title='View Room Details'  className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer' />
+            <div key={room._id} 
+            className='flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0'>
+              <img 
+              onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}}
+               src={room.images[0]} alt="Hotel-img" title='View Room Details' 
+                className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer' />
                 <div className='md:w-1/2 flex flex-col gap-2'>
                    <p className='text-gray-500'>{room.hotel.city}</p>
                    <p onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}}
@@ -75,7 +82,8 @@ const AllRooms = () => {
                       <div className='flex flex-wrap items-center mt-3 mb-3 gap-4'>
                         {room.amenities.map((item, index)=>(
                           <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70'>
-                             <img src={facilityIcons[item]} alt={item} className='w-5 h-5' />
+                             <img src={facilityIcons[item]} alt={item} 
+                             className='w-5 h-5' />
                              <p className='text-xs'>{item}</p>
                           </div>
                         ))}
@@ -126,4 +134,4 @@ const AllRooms = () => {
   )
 }
 
-export default AllRooms
+export default AllRooms;

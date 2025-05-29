783137a8149f1323bd19b3e745d240cb7954bc8c
waibaBikash <kavrelibikash@gmail.com>
2025-05-29 21:39:44 +1000

    Fix formatting issues in Home component by adding missing semicolons and ensuring consistent code style

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import FeatureDestination from '../components/FeatureDestination'
-import ExclusiveOffers from '../components/ExclusiveOffers'
-import Testimonial from '../components/Testimonial'
-import NewsLetter from '../components/NewsLetter'
+import Hero from '../components/Hero';
+import FeatureDestination from '../components/FeatureDestination';
+import ExclusiveOffers from '../components/ExclusiveOffers';
+import Testimonial from '../components/Testimonial';
+import NewsLetter from '../components/NewsLetter';
 
 const Home = () => {
   return (
@@ -17,4 +16,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;

06070337c02c818c10603ff7f20ff4bdb1b9f51a
waibaBikash <kavrelibikash@gmail.com>
2025-06-05 21:57:29 +1000

    Fix formatting issues in ListRoom component by improving indentation and ensuring consistent code style

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -39,8 +39,12 @@ const ListRoom = () => {
                              className='sr-only peer' 
                              checked={item.isAvailable} />
                               <div 
-                               className='w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200'></div>
-                                <span className='dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5'></span>
+                               className='w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200'>
+                               </div>
+                                <span 
+                                className='dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5'>
+
+                                </span>
 
                            </label>
                              
@@ -55,4 +59,4 @@ const ListRoom = () => {
   )
 }
 
-export default ListRoom
+export default ListRoom;
